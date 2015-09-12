5433f97c865457206c849210cc591de4df0dc813
jbenkual <joe@benkual.com>
2015-09-12 16:17:35 -0700

    Add mailgun node module to send emails
    Remove curl command from code

--- sendEmail.js
+++ sendEmail.js
@@ -1,12 +1,15 @@
 'use strict';
+var Mailgun = require("mailgun").Mailgun;
 var exec = require("child_process").exec;
 var child;
 var key = process.env.GM_API_KEY;
 
+let mg = new Mailgun(key);
+
 exports.sendEmail = function(dest, subject, message) {
 	let promise = new Promise((resolve, reject) => {
 		
-		child = exec(`
+		/*child = exec(`
 			curl -s --user 'api:${key}' \
 		    https://api.mailgun.net/v3/sandboxdc7bd586535b41cda8522138eed1ba22.mailgun.org/messages \
 		    -F from='Mailgun Sandbox <postmaster@sandboxdc7bd586535b41cda8522138eed1ba22.mailgun.org>' \
@@ -22,7 +25,17 @@ exports.sendEmail = function(dest, subject, message) {
 					reject();
 				}
 				resolve();
-			});
+			});*/
+	console.log("sending")
+		mg.sendText("postmaster@sandboxdc7bd586535b41cda8522138eed1ba22.mailgun.org", dest, subject, message, {}, (err) => {
+			if(err) {
+				console.log(err);
+				reject(err);
+			}
+			else {
+				resolve();
+			}
+		});
 	});
 	return promise;
 };
